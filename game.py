9422bd4b158fabe2d3a439ea8277834b83a85733
tonwell <wellington.pbi10@gmail.com>
2018-09-22 22:18:44 -0300

    #1 - Game Loop com evento de clique

---
+++ game.py
@@ -0,0 +1,19 @@
+import pygame
+from tabuleiro import Tabuleiro
+
+pygame.init()
+
+tela = pygame.display.set_mode((1024,768))
+fim = False
+mouseEsquerdo = 1
+
+while not fim:
+
+    for evento in pygame.event.get():
+        if evento.type == pygame.QUIT:
+            fim = True
+
+        if evento.type == pygame.MOUSEBUTTONDOWN and evento.button == mouseEsquerdo:
+            print("Clique esquerdo - posição (%d,%d)" % evento.pos)
+
+        pygame.display.flip()
