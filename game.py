61c5189b971432592ef157d726d5f5200f1b20c7
Edoarda Vallim <medoarda@id.uff.br>
2018-11-24 19:04:06 -0200

    #29 Implementação da tela de início

--- game.py
+++ game.py
@@ -4,14 +4,34 @@ from Interface import Interface
 
 pygame.init()
 
+
 fim = False
 mouseEsquerdo = 1
 
-bg = pygame.image.load(os.path.join("assets", "sprites", "background.png"))
+telaInicial = True
 
+# coisas de login
+tela = pygame.display.set_mode((640,480))
+sprite = pygame.image.load(os.path.join("assets", "sprites", "login.png"))
+tela.blit(sprite, (0, 0))
+
+while telaInicial and not fim:
+    for evento in pygame.event.get():
+        if evento.type == pygame.QUIT:
+            fim = True
+
+        if evento.type == pygame.MOUSEBUTTONDOWN and evento.button == mouseEsquerdo:
+            telaInicial = False
+
+        pygame.display.flip()
+
+
+#entrando no jogo
+# bg = pygame.image.load(os.path.join("assets", "sprites", "background.png"))
 interface = Interface.pegaInstancia()
 interface.cria()
 
+# game loop
 while not fim:
     for evento in pygame.event.get():
         if evento.type == pygame.QUIT:
